cd5456fab196f9a242493aa48f1967e335e4119c
sesn <sesankar11@gmail.com>
2017-03-03 15:11:18 +0530

    build: Vimeo channel videos fetch functionality added

--- index.php
+++ index.php
@@ -1,19 +1,52 @@
-<!doctype html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <title>Vimeo Javascript API</title>
-</head>
-<body>
-    <section id="container">
-        <h1>Vimeo API</h1>
-        <div id="results">
-            
-        </div>
-    </section>
+<?php 
+require_once('lib/Vimeo.php');
 
+$consumer_key = "7d56781df87ee448ef321ab19d7e8e93cabf8533";
+$consumer_secret_key = "TeYM3/6CBPLaVYPgroBuP7JGeuPox+DnRjqV5P9nD4pV8hbKhesE0xtrXhaKI17GIpfzIM9GRDUMysJI8jlor1utVIqdUYL/lvF/Lo7sIPoFAT/lgiUh0cvFYQ3ePfRW";
+$access_token = "ccac204f159c17ef2eb49903e6f5353a";
 
-<script src="https://code.jquery.com/jquery-1.12.4.min.js" integrity="sha256-ZosEbRLbNQzLpnKIkEdrPv7lOy9C27hHQ+Xp8a4MxAQ=" crossorigin="anonymous"></script>
-<script type="text/javascript" src="js/main.js"></script>
-</body>
-</html>
+$vimeo = new Vimeo($consumer_key, $consumer_secret_key, $access_token);
+
+$channel_video_array = [];
+
+$video_data_array =  get_channel_video_data();
+
+echo count($video_data_array);
+
+function get_channel_video_data($page_number = null) {
+
+    $video_endpoint_url = '/channels/nicetype/videos?per_page=100';
+
+    if($page_number != null)
+        $video_endpoint_url.'&page='.$page_number;
+    
+    $channel_data = $GLOBALS['vimeo']->request($video_endpoint_url);
+    $rate_ratelimit_remaining = $channel_data['headers']['X-RateLimit-Remaining'];
+
+    if($rate_ratelimit_remaining == 0):
+        echo "Rate limit exceeded. Please try again some time";
+        return false;
+    else:
+        $paging_object = (object) $channel_data['body']['paging'];
+        $video_array = $channel_data['body']['data'];
+        foreach($video_array as $video) {
+            array_push($channel_video_array, $video);
+        }
+        $next_page = getPageNumber($paging_object->next,'page');
+
+        if($next_page == null) {
+            return $channel_video_array;
+        } else {
+            get_channel_video_data($next_page);
+        }
+    endif;
+
+}
+
+function getPageNumber($url, $url_param) {
+    $parsed_url = parse_url($url);
+    parse_str($parsed_url['query'], $query);
+    return $query[$url_param];
+}
+
+?>
