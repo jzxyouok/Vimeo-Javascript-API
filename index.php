e758368a745fea9755b0bf471e54cc3a81a2db82
sesn <sesankar11@gmail.com>
2017-03-18 15:17:34 +0530

    fix: loop error

--- index.php
+++ index.php
@@ -1,9 +1,12 @@
 <?php 
 require_once('lib/Vimeo.php');
 
+//Sankar API
 $consumer_key = "7d56781df87ee448ef321ab19d7e8e93cabf8533";
 $consumer_secret_key = "TeYM3/6CBPLaVYPgroBuP7JGeuPox+DnRjqV5P9nD4pV8hbKhesE0xtrXhaKI17GIpfzIM9GRDUMysJI8jlor1utVIqdUYL/lvF/Lo7sIPoFAT/lgiUh0cvFYQ3ePfRW";
 $access_token = "ccac204f159c17ef2eb49903e6f5353a";
+
+
 
 $vimeo = new Vimeo($consumer_key, $consumer_secret_key, $access_token);
 
@@ -16,8 +19,9 @@ echo count($video_data_array);
 function get_channel_video_data($page_number = null) {
 
     $video_endpoint_url = '/channels/nicetype/videos?per_page=100';
+    // $video_endpoint_url = '/users/63133666/videos?per_page=100';
 
-    if($page_number != null)
+    if($page_number != null) {}
         $video_endpoint_url.'&page='.$page_number;
     
     $channel_data = $GLOBALS['vimeo']->request($video_endpoint_url);
@@ -30,15 +34,11 @@ function get_channel_video_data($page_number = null) {
         $paging_object = (object) $channel_data['body']['paging'];
         $video_array = $channel_data['body']['data'];
         foreach($video_array as $video) {
-            array_push($channel_video_array, $video);
+            array_push($GLOBALS['channel_video_array'], $video);
         }
         $next_page = getPageNumber($paging_object->next,'page');
 
-        if($next_page == null) {
-            return $channel_video_array;
-        } else {
-            get_channel_video_data($next_page);
-        }
+        return $GLOBALS['channel_video_array'];
     endif;
 
 }
